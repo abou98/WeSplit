d2f17ffcc9fe4f6af765e83c20edc7debf133a1a
Abou DIOP <abou.diop.98@gmail.com>
2023-02-06 18:17:15 +0000

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Abou DIOP on 06/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
